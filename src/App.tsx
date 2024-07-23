55f50ee09399aadd32b753b7deba2aced077dc6a
Roman Khassazhirov <roman.khassazhirov@gmail.com>
2024-07-23 18:34:55 +0300

    Added section Contacts. Component Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Header } from './layout/header/Header';
+import { Contact } from './layout/sections/contacts/Contacts';
 import { Main } from './layout/sections/main/Main';
 import { Projects } from './layout/sections/projects/Projects';
 import { Skills } from './layout/sections/skills/Skills';
@@ -11,6 +12,7 @@ function App() {
       <Main/>
       <Skills/>
       <Projects/>
+      <Contact/>
     </div>
   );
 }
---
+++ src/layout/sections/contacts/Contacts.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { SectionTitle } from "../../../compontens/SectionTitle";
+import { Button } from "../../../compontens/Button";
+
+export const Contact = () => {
+  return (
+    <StyledContacts>
+      <SectionTitle>Contacts</SectionTitle>
+      <StyledForm>
+        <label>Name</label>
+        <Field placeholder={"enter name"}/>
+        <label>Email</label>
+        <Field placeholder={"enter email"}/>
+        <label>Message</label>
+        <Field as={"textarea"} placeholder={"enter message"}/>
+        <Button type={"submit"}>Send</Button>
+      </StyledForm>
+    </StyledContacts>
+  );
+};
+
+const StyledContacts = styled.section`
+  min-height: 600px;
+  background-color: #a56969;
+`
+
+const StyledForm = styled.form`
+  margin: 0 auto;
+  max-width: 520px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+const Field = styled.input`
+  &:not(:last-child) {
+    margin-bottom: 25px;
+  }
+`
